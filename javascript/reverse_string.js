bb450daccd6ccab2e53a1024b2e553591872eba3
Joshua Campos <jdeveloper1203@gmail.com>
2022-12-12 13:11:23 -0500

    commit tree 722f21125cddb82cab3d993b37391e607d9dd3d7

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,16 @@
 function reverseString(str) {
   // type your code here
+
+  let result = [];
+
+  for (let i = str.length - 1; i > -1; i--) {
+    result.push(str[i]);
+
+  }
+
+  return result.join("");
+
+
 }
 
 if (require.main === module) {
@@ -11,9 +22,22 @@ if (require.main === module) {
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
+
+  console.log("Expecting: 'auhsoJ'");
+  console.log("=>", reverseString("Joshua"));
+
+  console.log("");
+
+  console.log("Expecting: 'Buggie'");
+  console.log("=>", reverseString("eigguB"));
 }
 
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// Initialize an empty array called result
+// iterate through each letter in str
+// push each letter of the string into the result array
+// return result.join("")
--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,5 +1,22 @@
 def reverse_string(str)
   # type your code in here
+
+  string = str.split('')
+  index = string.length
+  result = []
+
+  
+
+  
+
+index.times do
+    result << string[index - 1]
+    index -= 1
+  end
+
+  return result.join('')
+
+  
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -16,3 +33,10 @@ end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+
+# initialize a string variable, storing the split of the str
+# initialize a index variable, storing the string length
+# initialize an empty array
+# a times for loop that will run value store in the index variable, 
+# and will run the line where the string[index - 1] will be appended into the array
+# then return the result array with a join('')
